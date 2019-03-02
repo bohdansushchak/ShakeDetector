6ace82e73bb377369fe341647d3149f4da04bfd8
Bohdan Sushchak <bogdansushak@gmail.com>
2019-03-02 13:00:52 +0200

    Add launcher icon

--- app/src/main/java/bohdan/sushchak/shakedetector/MainActivity.java
+++ app/src/main/java/bohdan/sushchak/shakedetector/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.textview);
-        
+
         mShakeObservable = ShakeDetector.create(this);
     }
 
